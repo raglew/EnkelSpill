51c73bdb154c0d26651cb7b9624897325552efc5
raglew <raglew@gmail.com>
2016-03-10 18:41:42 +0100

    Lagt til python fil TicTacToe.py

---
+++ TicTacToe.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Tic Tac Toe med 2 spillere
+
+import turtle as t
+
+# 1. Tegne grid
+# 2. Spiller 1 plassere
+# 3. Spiller 2 plassere
+# 4. Inn til en av spillene winner
+
+
+t.mainloop()
