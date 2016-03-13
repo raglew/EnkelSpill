fdaa19629b649f9b979bdd7aa376506f47a555ad
raglew <raglew@gmail.com>
2016-03-13 20:30:51 +0100

    Lagt til 'svitsj spillere' funksjon.

--- TicTacToe.py
+++ TicTacToe.py
@@ -84,12 +84,21 @@ def hvem_starter():
              random.randrange(1, 3) henter et tall i listen [1, 2]
               En liste fra 1 til 3, men ikke inkludert 3, der altså.        
     --------------------------------------------------- Jonas ---- """
-    spiller = random.randrange(1, 3)
-    if spiller == 1:
-        print("Spiller 1 starter.")
+
+    # 1. endret variabel 'spiller' til 'player' for å unngå 'shadowing in outer scope'
+    # 2. lagt til variabel neste_player slik at vi kan svitsjer mellom spillere
+
+    # ---------------------------------------raglew---------------------------------
+
+    player = random.randrange(1, 3)
+    if player == 1:
+        next_player = 2
+        print("Spiller %s starter." % player)                                           # debug data
     else:
-        print("Spiller 2 starter.")
-    return spiller
+        player = 2
+        next_player = 1
+        print("Spiller %s starter." % player)                                           # debug data
+    return player, next_player
 
 
 def hvem_sin_tur():
@@ -109,13 +118,30 @@ def hvem_sin_tur():
 
 
 def plassere(x, y):
+    # ------  funksjon for å behandle spillerens klikk og tegne et figur  ----   raglew
+
     print(x, y)                                                                             # debug data
-    # må lage en funksjon som behandle spillerens klikk                                     raglew
+
+
+def svitsj_spillere(player, next_player):
+    # --------  funksjon for å svitsje spillere
+
+    print('før svitsj i fn', player, next_player)                                                   # debug data
+
+    player, next_player = next_player, player
+
+    print('etter svitsj i fn', player, next_player)                                                  # debug data
+
+    return player, next_player
+
 
 # Init graphics and logics
 tegne_grid()
 tegne_rutenummer()
-spiller = hvem_starter()
+
+# -------- endret for å hent både spiller og neste spiller ---- raglew
+spiller, neste_spiller = hvem_starter()
+print('spiller, neste spiller', spiller, neste_spiller)                                          # debug data
 
 # Main game
 hvem_sin_tur()
@@ -124,7 +150,13 @@ hvem_sin_tur()
 
 s.onclick(plassere)
 
-# 3. Veksler spillere
+# 3. Svitsj spillere
+
+print('før svitsj', spiller, neste_spiller)                                                   # debug data
+
+spiller, neste_spiller = svitsj_spillere(spiller, neste_spiller)
+
+print('etter svitsj', spiller, neste_spiller)                                                   # debug data
 
 # 3. Spiller 2 plassere
 
