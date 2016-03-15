4589b380472618d7f5e6717b1afa5c243c3ed744
Jonas Solsvik <jonas.solsvik@gmail.com>
2016-03-15 03:32:22 +0100

    Versjon 0.9 - megaupdate
      Kommentar: Denne updaten synes jeg blir alt for stor.
                  Jeg burde "git commit" oftere, slik at jeg kunne hatt
                  flere updates, med mindre forandringer per update.
    
        - Lagt til logikk som kårer en vinner i den nye f = hvem_vinner().
        - Lagt til en beskjed som viser vinneren i den nye f = run_victory().
        - Lagt til f = new_game(), og plassert mye eksisterende ting inn
          i denne.
        - Lagt til f = init_turtles()
        - Har tydeliggjort forskjellen på globale KONSTANTER og
           globale variabler.
    
    Issue #1 : Mangler en metode for å håndtere uavgjort på.
    Issue #2 : Spillet vil av og til dobbeltkjøre run_victory()

--- TicTacToe.py
+++ TicTacToe.py
@@ -17,34 +17,11 @@ f = Turtle()                     # f = figur, for å tegne figur etter spilleren
 i = Turtle()                     # i = instruks, for å endre instruks i hvem_sin_tur funksjon -- raglew
 w = Turtle()                     # w = warning, skriver warning dersom en rute er opptatt --Jonas 
 
-# Init turtle
-t.speed(8)
-t.ht()
 
-# -------------------init figur turtle  ------------  raglew
-f.ht()
-f.speed(0)
-f.shape('circle')                               # sirkel som test
-farge = 'red'                                   # variabel som kan brukes i svitsj_spiller funksjon
-f.color(farge)                                 # rød farge som test
-f.turtlesize(3)
-f.penup()
+# ------------------  KONSTANTER   -------------------------------
 
-# -------------------init instruks turtle ------------raglew
-i.ht()
-i.speed(0)
-i.penup()
-i.shape('circle')
-i.turtlesize(3)
-
-# ---------------Init warning turtle -------------Jonas
-w.ht()
-w.speed(0)
-w.color("Orange")
-
-# --- Finne window boundries --- different on each machine ---
-win_w = s.window_width()
-win_h = s.window_height()
+win_w = s.window_width()         # Forskjellig for hver maskin
+win_h = s.window_height()        # -----------// -------------
 
 # --- Sette grensene for koordinatsystemet ---
 max_x = win_w/2
@@ -64,13 +41,40 @@ midtirute = {   7 : [min_x + (3/10*win_w), min_y + (7/10*win_h)],
                 2 : [min_x + (5/10*win_w), min_y + (3/10*win_h)],
                 3 : [min_x + (7/10*win_w), min_y + (3/10*win_h)]   }
 
-# --- Denne listen lagrer ruter som er opptatt --- Jonas
+# --------------- GLOBALE VARIABLER -------------------------------
 opptatt_rute = []
+rutefarger = {}
+spiller, neste_spiller = 0, 0
+farge = ""
 
 
-# 1. Tegne grid
+# ----------------- FUNKSJONER --------------------------
+
+def init_turtles():
+    # ----------------- First turtle -------------
+    t.speed(8)
+    t.ht()
+    # -------------------init figur turtle  ------------  raglew
+    f.ht()
+    f.speed(0)
+    f.shape('circle')                        # sirkel som test
+    farge = 'red'                            # variabel som kan brukes i svitsj_spiller funksjon
+    f.color(farge)                           # rød farge som test
+    f.turtlesize(3)
+    f.penup()
+    # -------------------init instruks turtle ------------raglew
+    i.ht()
+    i.speed(0)
+    i.penup()
+    i.shape('circle')
+    i.turtlesize(3)
+    # ---------------Init warning turtle -------------Jonas
+    w.ht()
+    w.speed(0)
+    w.color("Orange")
+
+
 def tegne_grid():
-
     # --- Tegner TicTacToe-streker med relative koordinater ---
     t.penup()
     t.setpos(min_x + (3/5*win_w), max_y - (1/5*win_h))
@@ -115,7 +119,7 @@ def tegne_rutenummer():
             t.write(liste[liste_counter], move=False, align="center",
                     font=("Arial", 6, "bold"))
             liste_counter += 1
-
+    
 
 def hvem_starter():
     """  Her bruker vi biblioteket random, og funksjonen
@@ -142,7 +146,7 @@ def hvem_starter():
     return player, next_player
 
 
-def hvem_sin_tur(player):
+def hvem_sin_tur():
     """ Denne funksjonen skriver en tekst oppe i venstre 
          hjørnet av spillet, som viser hvem sin tur det er. 
           Håpet er at denne skal oppdatere seg selv, etterhvert 
@@ -157,7 +161,7 @@ def hvem_sin_tur(player):
     # nyttig å bruke farge til gjeldene spiller :)
     i.color(farge)
     i.goto(min_x + (win_w * 0.5/10), max_y - (win_h * 1.2/10))
-    i.write("Spiller %d sin tur" % player, move=False, align="left",
+    i.write("Spiller %d sin tur" % spiller, move=False, align="left",
             font=("Arial", 20, "bold"))
     # - Added big circle in right corner
     i.goto(max_x - (win_w * 1/10), max_y - (win_h * 1/10))
@@ -187,7 +191,7 @@ def svitsj_spillere():
     print('etter svitsj i fn', spiller, neste_spiller, f.color())       # debug data
 
     # den svisjet spilleren kan nå plassere
-    hvem_sin_tur(spiller)
+    hvem_sin_tur()
 
 
 def plasser_stamp_onclick(x, y):
@@ -201,7 +205,6 @@ def plasser_stamp_onclick(x, y):
     print(x, y)                                             # debug data
     for i in range(1,10):
         midt_x, midt_y = midtirute[i]
-
         # h = sqrt((x1-x2)^2 + (y1 - y2)^2)  Ref: Pytagoras
         d = ((((midt_x - x)**2) + ((midt_y - y)**2))**0.5) 
         if d < 50:                                      # Treshhold < 50
@@ -210,33 +213,89 @@ def plasser_stamp_onclick(x, y):
 
 
 def tegn_stamp(rute):
-	""" Funksjonen får data om hvilken rute det skal tegnes i, og 
-	     henter posisjonen til midten av ruten utifra "midtirute = {", 
-	      som er definert i starten av programmet. Posisjon settes og
-	       det f.stamp()-es. Til slutt så startes svitsj_spillere().
-	        Update: Dersom ruten allerede er brukt, så vil funksjonen
-	         nekte å skrive noe, og starte tegn_opptatt().
+    """ Funksjonen får data om hvilken rute det skal tegnes i, og 
+         henter posisjonen til midten av ruten utifra "midtirute = {", 
+          som er definert i starten av programmet. Posisjon settes og
+           det f.stamp()-es. Til slutt så startes svitsj_spillere().
+            Update: Dersom ruten allerede er brukt, så vil funksjonen
+             nekte å skrive noe, og starte tegn_opptatt().
     -------------------------------------------- Jonas ----- """
-	global opptatt_rute
-	if rute in opptatt_rute:                   # "Er ruten opptatt ?"
-		print("Ruten er allrede opptatt!")
-		tegn_opptatt(rute)
-		return 0
-	else:
-		opptatt_rute.append(rute)              # "Denne ruten er nå opptatt"
-		midt_x, midt_y = midtirute[rute]
-		
-		f.setpos(midt_x, midt_y)
-		f.stamp()
-		svitsj_spillere()
+    global opptatt_rute
+    if rute in opptatt_rute:                   # "Er ruten opptatt ?"
+        print("Ruten er allrede opptatt!")
+        tegn_opptatt(rute)
+        return 0
+    else:
+        opptatt_rute.append(rute)              # "Denne ruten er nå opptatt"
+        midt_x, midt_y = midtirute[rute]
+        
+        f.setpos(midt_x, midt_y)
+        f.color(farge)
+        f.stamp()
+        if hvem_vinner(rute):
+            new_game()                         # LAUNCHES NEW GAME!
+            return 0
+        else:
+            svitsj_spillere()
 
 
 def tegn_opptatt(rute):
-	w.setpos(0, 0)
-	w.write("RUTE %d ER OPPTATT" % rute, move=False, align="center", 
-		    font=("Arial", 40, "bold"))
-	time.sleep(1.5)
-	w.clear()
+    w.write("RUTE %d ER OPPTATT" % rute, move=False, align="center", 
+            font=("Arial", 40, "bold"))
+    time.sleep(1.5)
+    w.clear()
+    
+
+def hvem_vinner(rute):
+    """For å kåre vinner så lagrer vi hvilken spiller
+        som har skrevet i hvilken rute, i rutefarger = {.
+         Variabelen --> farge, representerer spilleren til en 
+          hver tid. Spiller1 = 'red', Spiller2 = 'green'
+           rute sendes videre fra tegn_stamp()
+    ---------------------------------------Jonas--------"""
+    global rutefarger
+    rutefarger[rute] = farge
+    # Horisontal                                                
+    if rutefarger[1] == rutefarger[2] == rutefarger[3]:     # Rute 1, 2, 3 er lik 
+        run_victory()
+        return True
+    elif rutefarger[4] == rutefarger[5] == rutefarger[6]:   # Rute 4, 5, 6 er lik
+        run_victory()
+        return True
+    elif rutefarger[7] == rutefarger[8] == rutefarger[9]:   # Rute 7, 8, 9 er lik
+        run_victory()
+        return True
+    # Vertikal
+    elif rutefarger[1] == rutefarger[4] == rutefarger[7]:   # Rute 1, 4, 7 er lik
+        run_victory()
+        return True
+    elif rutefarger[2] == rutefarger[5] == rutefarger[8]:   # Rute 2, 5, 8 er lik
+        run_victory()
+        return True
+    elif rutefarger[3] == rutefarger[6] == rutefarger[9]:   # Rute 3, 6, 9 er lik
+        run_victory()
+        return True
+    # Diagonal
+    elif rutefarger[1] == rutefarger[5] == rutefarger[9]:   # Rute 1, 5, 9 er lik
+        run_victory()
+        return True
+    elif rutefarger[7] == rutefarger[5] == rutefarger[3]:   # Rute 7, 5, 3 er lik
+        run_victory()
+        return True
+    else:
+        print("Ingen har vunnet enda!")                    # debug data
+        return False
+
+def run_victory():
+    time.sleep(1)
+    s.clear()
+    w.color(farge)
+    w.write("SPILLER %d HAR VUNNET!" % spiller, move=False, align="center", 
+            font=("Arial", 40, "bold"))
+    print("Gratulerer spiller {0} !!".format(spiller))     # debug data
+    time.sleep(2)
+
+    # --- Start over again ---
 
 
 def tegn_stamp1():
